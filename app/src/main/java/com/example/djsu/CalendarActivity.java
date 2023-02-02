ae4d3b2328f31453cf6d6d51d247b8b7ec6b4088
ghans <ghans1000@gmail.com>
2023-02-02 21:56:49 +0900

    햄버거 적용

--- app/src/main/java/com/example/djsu/CalendarActivity.java
+++ app/src/main/java/com/example/djsu/CalendarActivity.java
@@ -3,24 +3,32 @@ package com.example.djsu;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class CalendarActivity extends AppCompatActivity {
 
     // 플로팅버튼 상태
     private boolean fabMain_status = false;
-
     private FloatingActionButton fabMain;
     private FloatingActionButton fabHealth;
     private FloatingActionButton fabFood;
 
     private FloatingActionButton fabKg;
-
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,52 @@ public class CalendarActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+//뒤로가기버튼 이미지 적용
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_hamburger);
+        navigationView = findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
 
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.home:
+                        Intent homeintent = new Intent(getApplicationContext(), main_user.class);
+                        startActivity(homeintent);
+                        return true;
+                    case R.id.calender:
+                        Intent calenderintent = new Intent(getApplicationContext(), CalendarActivity.class);
+                        startActivity(calenderintent);
+                        return true;
+                    case R.id.communety:
+                        Intent communetyintent = new Intent(getApplicationContext(), community.class);
+                        startActivity(communetyintent);
+                        return true;
+                    case R.id.mypage:
+                        Intent mypageintent = new Intent(getApplicationContext(), mypage.class);
+                        startActivity(mypageintent);
+                        return true;
+                    case R.id.map:
+                        Intent mapintent = new Intent(getApplicationContext(), map.class);
+                        startActivity(mapintent);
+                        return true;
+                   /* case R.id.manbogi:
+                        Intent manbogiintent = new Intent(getApplicationContext(), .class);
+                        startActivity(manbogiintent);
+                        return true;*/
+                    case R.id.annoucement:
+                        Intent annoucementintent = new Intent(getApplicationContext(), annoucement.class);
+                        startActivity(annoucementintent);
+                        return true;
+                }
+                return false;
+            }
+        });
         fabMain = findViewById(R.id.floatingMain);
         fabHealth = findViewById(R.id.floatingHealth);
         fabFood = findViewById(R.id.floatingFood);
@@ -103,7 +156,18 @@ public class CalendarActivity extends AppCompatActivity {
         }
         // 플로팅 버튼 상태 변경
         fabMain_status = !fabMain_status;
-    }
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
