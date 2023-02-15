d855b0cd272486df9898627a2f3d0fb6cf52c961
ghans <ghans1000@gmail.com>
2023-02-15 21:14:04 +0900

    운동 검색

--- app/src/main/java/com/example/djsu/Fragment/Fragment8.java
+++ app/src/main/java/com/example/djsu/Fragment/Fragment8.java
@@ -1,9 +1,12 @@
 package com.example.djsu.Fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.djsu.ButtonAdapter;
+import com.example.djsu.Food;
 import com.example.djsu.R;
 import com.example.djsu.exButtonAdapter;
 import com.example.djsu.exerciseAdapter;
@@ -23,24 +28,29 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Fragment8 extends Fragment {
     private FirebaseFirestore db;
     private RecyclerView recyclerView,recyclerView1;
-    private RecyclerView.Adapter adapter,buttonAdapter;
+    private RecyclerView.Adapter buttonAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private ArrayList arrayList;
+    private ArrayList<exerciseLsit> arrayList;
+    private ArrayList<exerciseLsit> search_list;
     private View view;
+    private EditText editText;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment8, container, false);
         recyclerView = view.findViewById(R.id.aerobic);
+        search_list = new ArrayList<>();
+        editText = view.findViewById(R.id.searchtext);
+
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-        arrayList = new ArrayList<exerciseLsit>();
 
+        arrayList = new ArrayList<>();
+        exerciseAdapter adapter = new exerciseAdapter(arrayList,getActivity());
         recyclerView1 = view.findViewById(R.id.aerobic2);
         recyclerView1.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getActivity());
@@ -48,11 +58,41 @@ public class Fragment8 extends Fragment {
 
         buttonAdapter = new exButtonAdapter(arrayList,getActivity(),getActivity());
         recyclerView1.setAdapter(buttonAdapter);
+
+
         db = FirebaseFirestore.getInstance();
 
-        adapter = new exerciseAdapter(arrayList,getActivity());
         recyclerView.setAdapter(adapter);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String searchText = editText.getText().toString();
+                search_list.clear();
+
+                if(searchText.equals("")){
+                    adapter.setItems(arrayList);
+                }
+                else {
+                    // 검색 단어를 포함하는지 확인
+                    for (int a = 0; a < arrayList.size(); a++) {
+                        if (arrayList.get(a).getExerciseName().toLowerCase().contains(searchText.toLowerCase())) {
+                            search_list.add(arrayList.get(a));
+                        }
+                        adapter.setItems(search_list);
+                    }
+                }
+            }
+
+        });
         db.collection("유산소")
                 .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
