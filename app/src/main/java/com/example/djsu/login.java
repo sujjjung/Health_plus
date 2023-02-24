809edbde37bf2b09d15fe6b426a8b765576a1d4f
ghans <ghans1000@gmail.com>
2023-02-24 16:49:43 +0900

    로그인

--- app/src/main/java/com/example/djsu/login.java
+++ app/src/main/java/com/example/djsu/login.java
@@ -2,6 +2,7 @@ package com.example.djsu;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,19 +12,27 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.djsu.admin.AdminMainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class login extends AppCompatActivity {
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener firebaseAuthListener;
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonLogIn;
-    private Button buttonSignUp;
+    private EditText et_id, et_pass;
+    private Button btn_login, btn_register;
 
     public static Context context_email;
 
@@ -33,66 +42,93 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        context_email = this;
+        et_id = findViewById(R.id.email_editText);
+        et_pass = findViewById(R.id.password_editText);
+        btn_login = findViewById(R.id.login_btn);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        editTextEmail = (EditText) findViewById(R.id.email_editText);
-        editTextPassword = (EditText) findViewById(R.id.password_editText);
-
-        buttonLogIn = (Button) findViewById(R.id.login_btn);
-        buttonLogIn.setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
-                    loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString());
-                } else {
-                    Toast.makeText(login.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+            public void onClick(View view) {
+                // EditText에 현재 입력되어있는 값을 get(가져온다)해온다.
+                String id = et_id.getText().toString();
+                String pw = et_pass.getText().toString();
 
-    public void loginUser(String id, String password) {
-        firebaseAuth.signInWithEmailAndPassword(id, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            if (id.equals("admin@test.com")) {
-                                Toast.makeText(login.this, "관리자 로그인 성공", Toast.LENGTH_SHORT).show();
-                                firebaseAuth.addAuthStateListener(firebaseAuthListener);
-
-                                Intent intent = new Intent(login.this, AdminMainActivity.class);
-                                startActivity(intent);
-                            } else{
-                                // 로그인 성공
-                                Toast.makeText(login.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            firebaseAuth.addAuthStateListener(firebaseAuthListener);
-                                ((user_information)getApplication()).setId(id);
-
-                            Intent intent = new Intent(login.this, main_user.class);
-                            intent.putExtra("email",id);
-                            startActivity(intent);
+                    public void onResponse(String response) {
+                        try {
+                            // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
+                            System.out.println("hongchul" + response);
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success && id.equals("admin")) { // 로그인에 성공한 경우
+                                String userName = jsonObject.getString("username");
+                                Toast.makeText(getApplicationContext(), "관리자로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                new BackgroundTask2().execute();
                             }
-                        } else {
-                            // 로그인 실패
-                            Toast.makeText(login.this, "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+
+                            else  if (success && id.equals("test") == false) {
+                                String name = jsonObject.getString("name");
+                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(login.this, main_user.class);
+                                intent.putExtra("name", name);
+                                startActivity(intent);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
                     }
-                });
-    }
+                };
+                LoginRequest loginrequest = new LoginRequest(id, pw, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(login.this);
+                queue.add(loginrequest);
+            }
+        });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (firebaseAuthListener != null) {
-            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+    class BackgroundTask2 extends AsyncTask<Void, Void, String> {
+        String target;
+        @Override
+        protected void onPreExecute() {
+            //List.php은 파싱으로 가져올 웹페이지
+            target = "http://119.197.11.177/userlist.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                URL url = new URL(target);//URL 객체 생성
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;StringBuilder stringBuilder = new StringBuilder();
+                while ((temp = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(temp + "\n");//stringBuilder에 넣어줌
+                }
+
+                //사용했던 것도 다 닫아줌
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();//trim은 앞뒤의 공백을 제거함
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(login.this, AdminMainActivity.class);
+            intent.putExtra("userlist",result);
+            startActivity(intent);
+            login.this.startActivity(intent);
         }
     }
 }
