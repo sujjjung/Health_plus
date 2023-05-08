84c3cefeeeda7766679fefaa211fca4d9769386d
sujjjung <enejd0613@gmail.com>
2023-05-09 01:27:53 +0900

    - 게시글 업로드 (이미지 내용이 안가는건 아직 해결 못함 ㅜ_ㅜ

--- app/src/main/java/com/example/djsu/community.java
+++ app/src/main/java/com/example/djsu/community.java
@@ -1,11 +1,23 @@
 package com.example.djsu;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -13,18 +25,74 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
 public class community extends AppCompatActivity {
+    // 햄버거
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+
+    // 게시글 출력 v1
+    // String profile, name, photo, content, date;
+    // private communityAdapter communityAdapter;
+    // private List<communityData> communityList;
+    // ArrayList<HashMap<String, String>> mArrayList;
+    // ListView mListView;
+    // String mJsonString;
+
+    // 게시글 출력v2
+    String myJSON;
+
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_CONTENT = "content";
+    private static final String TAG_ID = "id";
+    private static final String TAG_IMAGE = "image";
+    private static final String TAG_DATE = "date";
+    JSONArray peoples = null;
+    ArrayList<HashMap<String, String>> personList;
+    ListView list;
+
+    // 게시글 출력 v3
+    String content, id, image, date;
+
+    private PostAdapter postAdapter;
+    private List<Post> postList;
+    ArrayList<HashMap<String, String>> mArrayList;
+    ListView mlistView;
+    String mJsonString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
 
+        Intent intent = getIntent();
+
+        // 마이페이지
         Button mypage_btn = (Button) findViewById(R.id.mypage_btn);
         mypage_btn.setOnClickListener(new View.OnClickListener() {
 
@@ -35,6 +103,7 @@ public class community extends AppCompatActivity {
             }
         });
 
+        // 포스트 추가
         ImageButton plusBtn = (ImageButton) findViewById(R.id.button2);
         plusBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -45,6 +114,7 @@ public class community extends AppCompatActivity {
             }
         });
 
+        // 댓글
         // ImageButton commentButton=findViewById(R.id.chat_bubble_btn);
         // findViewById(R.id.button4) : 버튼의 text가 아니라 Id값을 넣어줘야한다.
         // commentButton.setOnClickListener(new View.OnClickListener() {
@@ -53,15 +123,16 @@ public class community extends AppCompatActivity {
         //        Intent intent=new Intent(getApplicationContext(),comment.class);
         //        startActivity(intent);
         //    }
+
+        // 햄버거
         toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-//뒤로가기버튼 이미지 적용
+        //뒤로가기버튼 이미지 적용
         actionBar.setHomeAsUpIndicator(R.drawable.ic_action_hamburger);
         navigationView = findViewById(R.id.navigationView);
         drawerLayout = findViewById(R.id.drawerLayout);
-
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -103,7 +174,90 @@ public class community extends AppCompatActivity {
                 return false;
             }
         });
+
+        // 게시글
+        list = (ListView) findViewById(R.id.community);
+        personList = new ArrayList<HashMap<String, String>>();
+        getData("http://enejd0613.dothome.co.kr/community_list.php");
     }
+
+    protected void showList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String content = c.getString(TAG_CONTENT);
+                String id = c.getString(TAG_ID);
+                String image = c.getString(TAG_IMAGE);
+                String date = c.getString(TAG_DATE);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_CONTENT, content);
+                persons.put(TAG_ID, id);
+                persons.put(TAG_IMAGE, image);
+                persons.put(TAG_DATE, date);
+
+                personList.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    community.this, personList, R.layout.item_community,
+                    new String[]{TAG_CONTENT, TAG_ID, TAG_IMAGE, TAG_DATE},
+                    new int[]{R.id.content, R.id.name_textView, R.id.photo, R.id.date }
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+    // 햄버거
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
@@ -114,5 +268,15 @@ public class community extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // 날짜
+    private String getTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String getTime = dateFormat.format(date);
+
+        return getTime;
     }
 }
