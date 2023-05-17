d163421f817fd9c237ad6e19ecc08590c1fb960a
Kim_Su_B <hbsubin1@naver.com>
2023-05-17 14:57:41 +0900

    일단 채팅 저장

---
+++ app/src/main/java/com/example/djsu/ChatRoomAdapter.java
@@ -0,0 +1,94 @@
+package com.example.djsu;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatRoomAdapter extends RecyclerView.Adapter<ChatRoomAdapter.MyViewHolder> {
+    private List<ChatData> mDataSet;
+    private String myNickName;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView TextView_msg;
+        public TextView TextView_nickname;
+
+        public TextView TextView_date;
+
+        public LinearLayout msgLinear, test2;
+
+        public View rootView;
+        public MyViewHolder(View v) {
+            super(v);
+            TextView_nickname = v.findViewById(R.id.TextView_nickname);
+            TextView_msg = v.findViewById(R.id.TextView_msg);
+            TextView_date = v.findViewById(R.id.TextView_date);
+            msgLinear = itemView.findViewById(R.id.itemId);
+            test2 = itemView.findViewById(R.id.itemId2);
+            rootView = v;
+        }
+    }
+
+    public ChatRoomAdapter(List<ChatData> myDataset, Context context, String myNickName) {
+        mDataSet = myDataset;
+        this.myNickName = myNickName;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ChatRoomAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_talk_mine, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ChatData chat = mDataSet.get(position);
+
+
+        holder.TextView_nickname.setText(chat.getUserName());
+        holder.TextView_msg.setText(chat.getMsg());
+        holder.TextView_date.setText(chat.getDate());
+
+        if(chat.getUserName().equals(this.myNickName)) {
+            holder.TextView_msg.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.TextView_nickname.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.TextView_date.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.msgLinear.setGravity(Gravity.RIGHT);
+            holder.test2.setGravity(Gravity.RIGHT);
+        }
+        else {
+            holder.TextView_msg.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.TextView_nickname.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.TextView_date.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.msgLinear.setGravity(Gravity.LEFT);
+            holder.test2.setGravity(Gravity.LEFT);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet == null ? 0 : mDataSet.size();
+    }
+
+    public ChatData getChat(int position) {
+        return mDataSet != null ? mDataSet.get(position) : null;
+    }
+
+    public void addChat(ChatData chat) {
+        mDataSet.add(chat);
+        notifyItemInserted(mDataSet.size()-1);
+
+    }
+}
--- app/src/main/java/com/example/djsu/chat_room.java
+++ app/src/main/java/com/example/djsu/chat_room.java
@@ -4,12 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.checkerframework.checker.units.qual.C;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class chat_room extends AppCompatActivity {
-
-    public chat_room(String chatRoomId) {
+    private RecyclerView mRecyclerView;
+    public RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<ChatData> chatList;
+    private EditText EditText_chat;
+    private Button Button_send;
+    private DatabaseReference myRef;
+    public static String getCurrentTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String currentTime = sdf.format(new Date());
+        return currentTime;
     }
 
     @Override
@@ -17,13 +44,77 @@ public class chat_room extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
-        Button BattleButton=findViewById(R.id.battle_btn);
-        //findViewById(R.id.button4) : 버튼의 text가 아니라 Id값을 넣어줘야한다.
-        BattleButton.setOnClickListener(new View.OnClickListener() {
+        Button_send = findViewById(R.id.btn_submit);
+        EditText_chat = findViewById(R.id.edt_message);
+
+        User user = new User();
+        String userId = user.getId();
+        String userName = user.getName();
+
+        Button_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),target.class);
-                startActivity(intent);
+                String msg = EditText_chat.getText().toString();
+                if(msg != null) {
+                    ChatData chat = new ChatData();
+                    chat.setUserName(userName);
+                    chat.setMsg(msg);
+
+                    // 현재 시간을 가져와서 ChatData 객체에 설정
+                    String currentTime = getCurrentTime();
+                    chat.setDate(currentTime);
+
+                    myRef.push().setValue(chat);
+                }
+            }
+        });
+
+        mRecyclerView = findViewById(R.id.recycler_messages);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        chatList = new ArrayList<>();
+        mAdapter = new ChatRoomAdapter(chatList, chat_room.this, userName);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Message");
+        //myRef = database.getReference("User").child(userId).child("Message");
+        //DatabaseReference myRef = database.getReference("Message");
+
+//        ChatData chat = new ChatData();
+//        chat.setUserName(userName);
+//        chat.setMsg("hi");
+//        myRef.setValue(chat);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                ChatData chat = snapshot.getValue(ChatData.class);
+                ((ChatRoomAdapter)mAdapter).addChat(chat);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
