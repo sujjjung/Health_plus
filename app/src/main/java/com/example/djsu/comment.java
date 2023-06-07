0691d9f09940a825e7b4ad7fffb7bcbcaa2d1737
sujjjung <enejd0613@gmail.com>
2023-06-07 11:36:02 +0900

    커뮤니티에 프로필 띄워또

--- app/src/main/java/com/example/djsu/comment.java
+++ app/src/main/java/com/example/djsu/comment.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -17,7 +19,6 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,8 +26,10 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -34,6 +37,7 @@ public class comment extends AppCompatActivity {
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+
     // 댓글 출력
     String myJSON;
 
@@ -45,6 +49,13 @@ public class comment extends AppCompatActivity {
     ArrayList<HashMap<String, String>> personList;
     ListView list;
 
+    // 수정된 부분: 댓글 작성에 필요한 변수 추가
+    private EditText et;
+    private Button post;
+    User user = new User();
+    private String userId = user.getId();
+    private int postId = 1; // 게시물 ID 불러오기
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,67 +65,46 @@ public class comment extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-//뒤로가기버튼 이미지 적용
         actionBar.setHomeAsUpIndicator(R.drawable.ic_action_hamburger);
         navigationView = findViewById(R.id.navigationView);
         drawerLayout = findViewById(R.id.drawerLayout);
 
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch(menuItem.getItemId()){
-                    case R.id.home:
-                        Intent homeintent = new Intent(getApplicationContext(), main_user.class);
-                        startActivity(homeintent);
-                        return true;
-                    case R.id.calender:
-                        Intent calenderintent = new Intent(getApplicationContext(), CalendarActivity.class);
-                        startActivity(calenderintent);
-                        return true;
-                    case R.id.communety:
-                        Intent communetyintent = new Intent(getApplicationContext(), community.class);
-                        startActivity(communetyintent);
-                        return true;
-                    case R.id.mypage:
-                        Intent mypageintent = new Intent(getApplicationContext(), mypage.class);
-                        startActivity(mypageintent);
-                        return true;
-                    case R.id.map:
-                        Intent mapintent = new Intent(getApplicationContext(), map.class);
-                        startActivity(mapintent);
-                        return true;
-                    case R.id.manbogi:
-                        Intent manbogiintent = new Intent(getApplicationContext(), pedometer.class);
-                        startActivity(manbogiintent);
-                        return true;
-                    case R.id.annoucement:
-                        Intent annoucementintent = new Intent(getApplicationContext(), annoucement.class);
-                        startActivity(annoucementintent);
-                        return true;
-                    case R.id.friend:
-                        Intent friend = new Intent(getApplicationContext(), chatList.class);
-                        startActivity(friend);
-                        return true;
+                switch (menuItem.getItemId()) {
+                    // navigation item 처리
                 }
                 return false;
             }
         });
+
         Intent intent = getIntent();
-        // 댓글
-        list = (ListView) findViewById(R.id.comment_list);
-        personList = new ArrayList<HashMap<String, String>>();
+
+        // 수정된 부분: 댓글 작성에 필요한 뷰 초기화
+        et = findViewById(R.id.et);
+        post = findViewById(R.id.post);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = et.getText().toString();
+                saveComment(content);
+            }
+        });
+
+        // 댓글 출력
+        list = findViewById(R.id.comment_list);
+        personList = new ArrayList<>();
         getData("http://enejd0613.dothome.co.kr/get_comments.php");
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -129,8 +119,7 @@ public class comment extends AppCompatActivity {
                 String created_at = c.getString(TAG_CREATED_AT);
                 String userId = c.getString(TAG_USERID);
 
-                HashMap<String, String> persons = new HashMap<String, String>();
-
+                HashMap<String, String> persons = new HashMap<>();
                 persons.put(TAG_CONTENT, content);
                 persons.put(TAG_CREATED_AT, created_at);
                 persons.put(TAG_USERID, userId);
@@ -141,46 +130,36 @@ public class comment extends AppCompatActivity {
             ListAdapter adapter = new SimpleAdapter(
                     comment.this, personList, R.layout.item_comment,
                     new String[]{TAG_CONTENT, TAG_CREATED_AT, TAG_USERID},
-                    new int[]{R.id.userComment, R.id.date, R.id.userName }
+                    new int[]{R.id.userComment, R.id.date, R.id.userName}
             );
 
-            // 리스트뷰에 어댑터 설정
             list.setAdapter(adapter);
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
 
     public void getData(String url) {
         class GetDataJSON extends AsyncTask<String, Void, String> {
-
             @Override
             protected String doInBackground(String... params) {
-
                 String uri = params[0];
 
-                BufferedReader bufferedReader = null;
                 try {
                     URL url = new URL(uri);
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     StringBuilder sb = new StringBuilder();
 
-                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
                     String json;
                     while ((json = bufferedReader.readLine()) != null) {
-                        sb.append(json + "\n");
+                        sb.append(json).append("\n");
                     }
 
                     return sb.toString().trim();
-
                 } catch (Exception e) {
                     return null;
                 }
-
-
             }
 
             @Override
@@ -191,6 +170,58 @@ public class comment extends AppCompatActivity {
         }
         GetDataJSON g = new GetDataJSON();
         g.execute(url);
+    }
 
+    // 수정된 부분: 댓글 저장을 위한 메소드 추가
+    private void saveComment(String content) {
+        class SaveCommentTask extends AsyncTask<Void, Void, String> {
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    String url = "http://enejd0613.dothome.co.kr/save_comment.php";
+                    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setDoOutput(true);
+
+                    // 댓글 데이터를 POST 요청으로 전송
+                    String postData = "content=" + URLEncoder.encode(content, "UTF-8");
+                    postData += "&userId=" + URLEncoder.encode(userId, "UTF-8");
+                    postData += "&postId=" + postId;
+
+                    OutputStream outputStream = connection.getOutputStream();
+                    outputStream.write(postData.getBytes("UTF-8"));
+                    outputStream.flush();
+                    outputStream.close();
+
+                    // 응답 받기
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    return response.toString().trim();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                if (result != null) {
+                    // 댓글 저장 성공
+                    // 댓글 리스트를 다시 가져와서 업데이트
+                    getData("http://enejd0613.dothome.co.kr/get_comments.php");
+                } else {
+                    // 댓글 저장 실패
+                }
+            }
+        }
+
+        SaveCommentTask saveTask = new SaveCommentTask();
+        saveTask.execute();
     }
 }
