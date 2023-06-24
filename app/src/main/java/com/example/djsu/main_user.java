97c15033be6abd428ebb9e37cbbcdf323a2a53fb
김종하 <ghans1000@gmail.com>
2023-06-24 12:15:23 +0900

    알

--- app/src/main/java/com/example/djsu/main_user.java
+++ app/src/main/java/com/example/djsu/main_user.java
@@ -58,6 +58,7 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
@@ -410,6 +411,7 @@ public class main_user extends AppCompatActivity {
             }
 
             uploadPicture(ID, getStringImage(bitmap));
+
         }
     }
 
